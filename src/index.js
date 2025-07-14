fad28b78348c79924e59ed92c0c943329aa9a376
Kazi-Irfanul-Islam <irfanulislam01851@gmail.com>
2025-07-14 18:07:26 +0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { StrictMode } from "react";
+import ReactDOM, { createRoot } from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="coder.jpg" alt="Coder-image" />;
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="HTML + CSS" emoji="💪" background="#1ab07cff" />
+      <Skill skill="JavaScript" emoji="⚡" background="yellow" />
+      <Skill skill="React" emoji="⚛️" background="Skyblue" />
+      <Skill skill="TypeScript" emoji="💪" background="orange" />
+      <Skill skill="Git and Github" emoji="🟢" background="red" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  const style = { backgroundColor: props.background };
+  return (
+    <div className="skill" style={style}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Kazi Irfanul Islam</h1>
+      <article>
+        I operate in the world of React, building scalable and responsive web
+        applications. My focus is on writing performant back-end logic and
+        dynamic front-end experiences that are both powerful and easy to use.
+      </article>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
